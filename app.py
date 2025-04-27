250433066b0bf43649b45195a4986f5b045dc5a9
Serhii Yasenev <serhii.yasenev@gmail.com>
2025-04-28 01:17:48 +0300

    upd

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ from flask import Flask
 from redis import Redis
 
 app = Flask(__name__)
-redis_url = os.getenv("REDIS_URL", "redis://redis:6379/0")
-redis = Redis.from_url(redis_url)
+redis_url = os.getenv("REDIS_URL")
+if redis_url is None or redis_url == "":
+    redis = Redis(host="redis", port=6379)
+else:
+    redis = Redis.from_url(redis_url)
 
 @app.route("/")
 def hello():
